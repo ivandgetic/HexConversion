7cde094fcac193fc783269b6722e4c30276e7a85
ivandgetic <ivandgetic@gmail.com>
2014-02-24 23:48:12 +0800

    Improve identical type conversion

--- app/src/main/java/ivandgetic/hexconversion/MainActivity.java
+++ app/src/main/java/ivandgetic/hexconversion/MainActivity.java
@@ -29,34 +29,37 @@ public class MainActivity extends Activity {
 
     public void conversion(View view) {
         if (Before_Binary.isChecked()) {
-            if (After_Octal.isChecked()) {//2转8
+            if (After_Binary.isChecked()) {//2转2
+                after_conversion.setText(before_conversion.getText().toString());
+            } else if (After_Octal.isChecked()) {//2转8
                 after_conversion.setText(Integer.toOctalString(Integer.parseInt(Integer.valueOf(before_conversion.getText().toString(), 2).toString())));
             } else if (After_Decimal.isChecked()) {//2转10
                 after_conversion.setText(Integer.valueOf(before_conversion.getText().toString(), 2).toString());
             } else if (After_Hex.isChecked()) {//2转16
                 after_conversion.setText(Integer.toHexString(Integer.parseInt(Integer.valueOf(before_conversion.getText().toString(), 2).toString())));
             }
-
         }
         if (Before_Octal.isChecked()) {
             if (After_Binary.isChecked()) {//8转2
                 after_conversion.setText(Integer.toBinaryString(Integer.parseInt(Integer.valueOf(before_conversion.getText().toString(), 8).toString())));
+            } else if (After_Octal.isChecked()) {//8转8
+                after_conversion.setText(before_conversion.getText().toString());
             } else if (After_Decimal.isChecked()) {//8转10
                 after_conversion.setText(Integer.valueOf(before_conversion.getText().toString(), 8).toString());
             } else if (After_Hex.isChecked()) {//8转16
                 after_conversion.setText(Integer.toHexString(Integer.parseInt(Integer.valueOf(before_conversion.getText().toString(), 8).toString())));
             }
-
         }
         if (Before_Decimal.isChecked()) {
             if (After_Binary.isChecked()) {//10转2
                 after_conversion.setText(Integer.toBinaryString(Integer.parseInt(before_conversion.getText().toString())));
             } else if (After_Octal.isChecked()) {//10转8
                 after_conversion.setText(Integer.toOctalString(Integer.parseInt(before_conversion.getText().toString())));
+            } else if (After_Decimal.isChecked()) {//10转10
+                after_conversion.setText(before_conversion.getText().toString());
             } else if (After_Hex.isChecked()) {//10转16
                 after_conversion.setText(Integer.toHexString(Integer.parseInt(before_conversion.getText().toString())));
             }
-
         }
         if (Before_Hex.isChecked()) {
             if (After_Binary.isChecked()) {//16转2
@@ -65,11 +68,9 @@ public class MainActivity extends Activity {
                 after_conversion.setText(Integer.toOctalString(Integer.parseInt(Integer.valueOf(before_conversion.getText().toString(), 16).toString())));
             } else if (After_Decimal.isChecked()) {//16转10
                 after_conversion.setText(Integer.valueOf(before_conversion.getText().toString(), 16).toString());
+            } else if (After_Hex.isChecked()) {//16转16
+                after_conversion.setText(before_conversion.getText().toString());
             }
-
         }
-
     }
-
-
 }
